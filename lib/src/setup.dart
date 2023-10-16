fbfe458d12d608b4a845627cb03c3e629807b155
SRB-acer <dev.chresyr@gmail.com>
2023-10-16 22:42:13 +0200

    Removed .perft files

--- lib/src/setup.dart
+++ lib/src/setup.dart
@@ -1,5 +1,4 @@
 import 'package:meta/meta.dart';
-import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'dart:math' as math;
 import './square_set.dart';
 import './models.dart';
